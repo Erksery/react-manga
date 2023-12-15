43a327bd2a44436b37eb81132ca5e866169693ac
Erksery <114258961+Erksery@users.noreply.github.com>
2023-12-15 20:42:11 +0300

    asdsd

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -8,10 +8,30 @@ function UserPage() {
   return (
     <BaseComponent>
       <div className={styles.userPage}>
-        <h2>{userData.loginUser}</h2>
-        {bookMarks.map((manga) => (
-          <MangaCard key={manga.idBookMarks} {...manga} />
-        ))}
+        <div
+          style={{
+            backgroundImage:
+              "url(http://localhost:5001/image/a15544ce2ff567d3a3a0ae3dc479d73577b08ac4_46368266003702174)",
+          }}
+          className={styles.userBanner}
+        />
+        <div className={styles.userInfoContainer}>
+          <div className={styles.groupInfo}>
+            <div className={styles.logoContainer}>
+              <div className={styles.userImage}>
+                <img src="http://localhost:5001/image/MqLYFST4k4mY_250x350" />
+              </div>
+            </div>
+            <div className={styles.userNameContainer}>
+              <h3>{userData.loginUser}</h3>
+            </div>
+          </div>
+        </div>
+        <div className={styles.bookMarksList}>
+          {bookMarks.map((manga) => (
+            <MangaCard key={manga.idBookMarks} {...manga} />
+          ))}
+        </div>
       </div>
     </BaseComponent>
   );
